9227f409505a92f1bfa9924b7c96055f87ec88d4
Steffen Kolb <steffen@17k.de>
2015-07-09 18:56:38 +0200

    REMOVED unnecessary files

--- dest/js/script.js
+++
@@ -1,6 +0,0 @@
-function greeter(person) {
-    return "Hello, " + person.firstname + " " + person.lastname;
-}
-var user = { firstname: "Jane", lastname: "User" };
-document.body.innerHTML = greeter(user);
-//# sourceMappingURL=app.js.map
